310323afd7c70c4f8d5fd7e9618244031a77a8be
Gogs <gogs@fake.local>
2018-05-10 10:50:38 +0200

    update

--- src/window.js
+++ src/window.js
@@ -2,37 +2,3 @@ window.on = window.addEventListener;
 document.on = document.addEventListener;
 window.off = window.removeEventListener;
 document.off = document.removeEventListener;
-
-
-// var _timersOne = [];
-// var _timers = [];
-
-// var _setUnload = false;
-
-// function destroyTimers() {
-//   alert("clear " + _timers.length + " " + _timersOne.length);
-//   for (let i = 0; i < _timers.length; i++) {
-//     clearTimeout(_timers[i]);
-//   }
-//   for (let i = 0; i < _timersOne.length; i++) {
-//     clearTimeout(_timersOne[i]);
-//   }
-// }
-
-
-
-// window.timerOne = function(fn, ms) {
-//   if (!_setUnload) {
-//     _setUnload = true;
-//     window.addEventListener("unload", () => { destroyTimers(); });
-//   }
-//   _timersOne.push(window.setTimeout(fn, ms));
-// }
-
-// window.timer = function(fn, ms) {
-//   if (!_setUnload) {
-//     _setUnload = true;
-//     window.addEventListener("unload", () => { destroyTimers(); });
-//   }
-//   _timers.push(window.setInterval(fn, ms));
-// }
